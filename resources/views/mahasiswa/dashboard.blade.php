ecda021d43cc2703c6d97e70733c8989fd986e70
NabilAchmad <nabilachmad316@gmail.com>
2025-05-14 09:33:23 +0700

    commit

--- resources/views/mahasiswa/dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.template.kaprodi')
-
-@section('content')
-<div class="container mt-5">
-    <h1>Mahasiswa Dashboard</h1>
-    <p>Welcome to the Mahasiswa dashboard after login.</p>
-</div>
-@endsection
